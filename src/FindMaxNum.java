4e4df9bee3be8928009772bb32743ab1d22dbe2e
정진규 <rritjy@naver.com>
2023-03-15 19:23:22 +0900

    격자판 최대값과 좌표찾기

--- src/FindMaxNum.java
+++ src/FindMaxNum.java
@@ -20,26 +20,41 @@ import java.util.stream.Stream;
 <출력예시>
 90
 5 7
-*/
+ */
 public class FindMaxNum {
 	private static int[][] arr = {{3,23,85,34,17,74,25,52,65},
-										{10,7,39,42,88,52,14,72,63},
-										{87,42,18,78,53,45,18,84,53},
-										{34,28,64,85,12,16,75,36,55},
-										{21,77,45,35,28,75,90,76,1},
-										{25,87,65,15,28,11,37,28,74},
-										{65,27,75,41,7,89,78,64,39},
-										{47,47,70,45,23,65,3,41,44},
-										{87,13,82,38,31,12,29,29,80}};
+			{10,7,39,42,88,52,14,72,63},
+			{87,42,18,78,53,45,18,84,53},
+			{34,28,64,85,12,16,75,36,55},
+			{21,77,45,35,28,75,90,76,1},
+			{25,87,65,15,28,11,37,28,74},
+			{65,27,75,41,7,89,78,64,39},
+			{47,47,70,45,23,65,3,41,44},
+			{87,13,82,38,31,12,29,29,80}};
 	public static void main(String[] args) {
-		int max=Stream.of(arr).flatMapToInt(Arrays::stream).max().getAsInt();
-		String s = "";
+		//		int max=Stream.of(arr).flatMapToInt(Arrays::stream).max().getAsInt();
+		//		String s = "";
+		//		for(int i=0; i<arr.length; i++) {
+		//				for(int j=0; j<arr[i].length; j++) {
+		//					if(max==arr[i][j])  s=(i+1)+" "+(j+1);
+		//			}
+		//		}
+		//		System.out.println(max+"\n"+s);
+
+		int max = arr[0][0];
+		int row=0, col=0;
 		for(int i=0; i<arr.length; i++) {
-				for(int j=0; j<arr[i].length; j++) {
-					if(max==arr[i][j])  s=(i+1)+" "+(j+1);
+			for(int j=0; j<arr.length; j++) {
+				if(max<arr[i][j]) {
+					max=arr[i][j];
+					row=i+1;
+					col=j+1;
+				}
 			}
 		}
-		System.out.println(max+"\n"+s);
-		
+		System.out.println(max+"\n"+row+" "+col);
+
 	}
+
+
 }
